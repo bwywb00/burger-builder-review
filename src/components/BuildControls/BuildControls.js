97dddef4d25621c783e1cc3f636f8c4cb7fa5f99
bwywb00 <bwywb00@gmail.com>
2020-05-06 21:21:16 +0900

    15. Create BuildControls Component

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const buildControls = (props) => {
+    return (
+        <div>
+            <div><span>치즈</span><span>-</span><span>+</span></div>
+            <div><span>고기</span><span>-</span><span>+</span></div>
+            <div><span>샐러드</span><span>-</span><span>+</span></div>
+            <div><span>베이컨</span><span>-</span><span>+</span></div>
+        </div>
+    )
+}
+
+export default buildControls;
