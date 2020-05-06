6f8968d4846ad8a8bcd13d732a0b03d006f23296
bwywb00 <bwywb00@gmail.com>
2020-05-06 21:01:26 +0900

    14. Dynamic Burger Ingredients

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -7,8 +7,6 @@ class BurgerIngredient extends Component {
     render() {
         let ingredient = null;
 
-        console.log(this.props.type);
-
         switch (this.props.type) {
             case ('bread-bottom'):
                 ingredient = <div className={classes.BreadBottom}></div>;
