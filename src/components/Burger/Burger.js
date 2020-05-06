351be1591aeafa90d48ca443f969791c79c3c2f5
bwywb00 <bwywb00@gmail.com>
2020-05-06 19:14:16 +0900

    9. Create Burger Component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const burger = (props) => {
+    return (
+        <div>
+            <div>빵</div>
+            <div>고기</div>
+            <div>고기</div>
+            <div>고기</div>
+            <div>고기</div>
+            <div>빵</div>
+        </div>
+    );
+}
+
+export default burger;
