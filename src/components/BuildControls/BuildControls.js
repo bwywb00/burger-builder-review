9c38185063dc1107139b53e9721dddd444e1c824
bwywb00 <bwywb00@gmail.com>
2020-05-06 21:26:29 +0900

    16. Create BuildControls.css

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './BuildControls.css';
 
 const buildControls = (props) => {
     return (
-        <div>
+        <div className={classes.BuildControls}>
             <div><span>치즈</span><span>-</span><span>+</span></div>
             <div><span>고기</span><span>-</span><span>+</span></div>
             <div><span>샐러드</span><span>-</span><span>+</span></div>
