da04c934e67547f1cf09700cb020b7c4b0673d1b
bwywb00 <bwywb00@gmail.com>
2020-05-12 19:25:25 +0900

    30. Creating a custom Button Comp && Implement

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
 
@@ -19,6 +20,18 @@ const orderSummary = (props) => {
             <p>A delicious burger with the following ingredients:</p>
             <ul>{ingredientSummary}</ul>
             <p>Continue to Check out?</p>
+            <Button
+                btnType="Danger"
+                clicked={props.purchaseCancelled}
+            >
+                CANCEL
+            </Button>
+            <Button
+                btnType="Success"
+                clicked={props.purchaseContinued}
+            >
+                CONTINUE
+            </Button>
         </Aux>
     );
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,6 +85,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
 
+    purchaseContinueHandler = () => {
+        alert('You Contiune');
+    }
+
     render() {
 
         const disableInfo = {
@@ -101,7 +105,11 @@ class BurgerBuilder extends Component {
                     show={this.state.purchasing}
                     modalClosed={this.purchaseCancelHandler}
                 >
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
