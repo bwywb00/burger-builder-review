93d51f47073a58dacc927aceb79ba9442699faf0
bwywb00 <bwywb00@gmail.com>
2020-05-31 22:34:18 +0900

    54. Order Submission & Passing Data BetWeen Pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,42 +100,13 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({
-        //     loading: true
-        // })
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice.toFixed(2),
-        //     customer: {
-        //         name: 'Sung',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCode: '41351',
-        //             country: 'Korea',
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     devliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({
-        //             loading: false,
-        //             purchasing: false
-        //         });
-        //     })
-        //     .catch(error => {
-        //         this.setState({
-        //             loading: false,
-        //             purchasing: false
-        //         });
-        //     });
 
         // query를 사용해서 state.ingredient 값을 전달하기
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice.toFixed(2));
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
@@ -10,7 +11,41 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({
+            loading: true
+        })
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Sung',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Korea',
+                },
+                email: 'test@test.com'
+            },
+            devliveryMethod: 'fastest'
         }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({
+                    loading: false
+                });
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({
+                    loading: false
+                });
+            });
     }
 
     render() {
@@ -22,7 +57,7 @@ class ContactData extends Component {
                     <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
                     <input className={classes.Input} type="text" name="street" placeholder="Street" />
                     <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success">ORDER</Button>
+                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
             </div>
         );
